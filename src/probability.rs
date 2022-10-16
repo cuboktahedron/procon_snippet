e8e2f775d99d515d181a2111e8f49b72048cc43f
cuboktahedron <cuboktahedron@gmail.com>
2022-10-16 10:58:22 +0900

    Add combination_dup_mod2

--- src/probability.rs
+++ src/probability.rs
@@ -31,6 +31,20 @@ mod probability {
     (((fact_n * fact_r1) % md) * fact_nr1) % md
   }
 
+  #[snippet(include = "pow_mod")]
+  fn combination_mod2(n: usize, m: usize, md: usize) -> usize {
+    let m = m.min(n - m);
+    let mut a = 1usize;
+    let mut b = 1usize;
+
+    for i in 0..m {
+      a = (a * (n - i)) % md;
+      b = (b * (i + 1)) % md;
+    }
+
+    return (a * pow_mod(b, md - 2, md)) % md;
+  }
+
   #[snippet(include = "combination_mod")]
   fn combination_dup_mod(n: usize, k: usize, md: usize) -> usize {
     combination_mod(n + k - 1, k, md)
@@ -54,6 +68,11 @@ mod probability {
       assert_eq!(combination_mod(8, 3, 31), 25);
     }
 
+    #[test]
+    fn test_combination_mod2() {
+      assert_eq!(combination_mod2(1, 1, 31), 1);
+      assert_eq!(combination_mod2(8, 5, 31), 25);
+    }
 
     #[test]
     fn test_combination_dup_mod() {
